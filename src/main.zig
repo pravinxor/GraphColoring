c9c1751ad960b1b94bdc72251db7341f8bff92b8
Pravin Ramana <pravinramana25@protonmail.ch>
2023-04-01 21:58:04 -0500

    Update main function to support these changes (and print out the results when possible)

--- src/main.zig
+++ src/main.zig
@@ -4,12 +4,12 @@ const ajlist = @import("adjacencylist.zig");
 pub fn main() !void {}
 
 test "initialize adjacency list" {
-    var list = try ajlist.AdjacencyList.init(std.testing.allocator, 5);
+    var list = try ajlist.AdjacencyList.init(std.testing.allocator, 5, null);
     defer list.deinit();
 }
 
 test "inserting edges into the adjacency list" {
-    var list = try ajlist.AdjacencyList.init(std.testing.allocator, 5);
+    var list = try ajlist.AdjacencyList.init(std.testing.allocator, 5, null);
     defer list.deinit();
 
     try list.insertEdge(1, 2);
@@ -18,17 +18,12 @@ test "inserting edges into the adjacency list" {
 }
 
 test "print function" {
-    var list = try ajlist.AdjacencyList.init(std.testing.allocator, 5);
+    var list = try ajlist.AdjacencyList.init(std.testing.allocator, 5, ajlist.ListGenerator.Type.cycle);
     defer list.deinit();
 
-    try list.insertEdge(1, 2);
-    try list.insertEdge(2, 3);
-    try list.insertEdge(0, 4);
+    var buf = std.ArrayList(u8).init(std.testing.allocator);
+    defer buf.deinit();
 
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try list.print(stdout);
-    try bw.flush();
+    try list.print(buf.writer());
+    std.debug.print("{c}{s}", .{ '\n', buf.items });
 }
