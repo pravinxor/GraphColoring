f42db6da5c15252caf8db92fcf4360502a47217c
Pravin Ramana <pravinramana25@protonmail.ch>
2023-05-07 18:52:41 -0500

    main driver code to print graphs

--- src/main.zig
+++ src/main.zig
@@ -4,7 +4,35 @@ const dlist = @import("degreelist.zig");
 const vertice = @import("vertice.zig");
 var stderr = std.io.getStdErr().writer();
 
-pub fn main() !void {}
+pub fn main() !void {
+    const allocator = std.heap.c_allocator;
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+    std.debug.print("Arguments: {s}\n", .{args});
+
+    const out = std.io.getStdOut();
+    var buf = std.io.bufferedWriter(out.writer());
+    var writer = buf.writer();
+
+    var vertices = try std.fmt.parseInt(u16, args[1], 10);
+    var collisions = try std.fmt.parseInt(u32, args[2], 10);
+    var generator: ?ajlist.ListGenerator.Type = null;
+    if (std.mem.eql(u8, args[3], "cycle")) {
+        generator = ajlist.ListGenerator.Type.cycle;
+    } else if (std.mem.eql(u8, args[3], "complete")) {
+        generator = ajlist.ListGenerator.Type.complete;
+    } else if (std.mem.eql(u8, args[3], "uniform")) {
+        generator = ajlist.ListGenerator.Type.random_uniform;
+    } else if (std.mem.eql(u8, args[3], "skewed")) {
+        generator = ajlist.ListGenerator.Type.random_skewed;
+    } else if (std.mem.eql(u8, args[3], "qskewed")) {
+        generator = ajlist.ListGenerator.Type.random_square;
+    }
+
+    var list = try ajlist.AdjacencyList.init(allocator, vertices, generator, collisions);
+    defer list.deinit();
+    try list.print(&writer);
+}
 
 test "initialize adjacency list" {
     var list = try ajlist.AdjacencyList.init(std.testing.allocator, 5, null, null);
